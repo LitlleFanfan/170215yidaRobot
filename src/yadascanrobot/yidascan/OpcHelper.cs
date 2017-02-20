62f28f32723e142be91301861d729e916ed64284
laozhang.gz <laozhang.gz@gmail.com>
2017-02-20 12:11:49 +0800

    modify opchelper

--- src/yadascanrobot/yidascan/OpcHelper.cs
+++ src/yadascanrobot/yidascan/OpcHelper.cs
@@ -1,87 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using ProduceComm.OPC;
 using yidascan.DataAccess;
 
-namespace yidascan {    
+namespace yidascan {
     /// <summary>
     /// 这个要合并修改。
     /// </summary>
     [Obsolete("待重构, 2013-02-17")]
     public static class OpcHelper {
-        static OPCClient client = null;
-        static OPCParam param = null;
-        
-        /// <summary>
-        /// 读布尔值。
-        /// </summary>
-        /// <param name="slot">地址</param>
-        /// <returns>非bool值时，弹出异常。</returns>
-        public static bool ReadBool(string slot) {
-            return (bool)client.Read(slot);
-        }
-
-        public static string ReadString(string slot) {
-            return (string)client.Read(slot);
-        }
-
-        public static decimal ReadDecimal(string slot) {
-            return (decimal)client.Read(slot);
-        }
-
-        public static  int ReadInt(string slot) {
-            return (int)client.Read(slot);            
-        }
-
-        public static string ReadACAreaLabel(LCodeSignal signal) {
+        public static string ReadACAreaLabel(IOpcClient client, LCodeSignal signal) {
             const int MAX_LEN = 6;
-            string r1 = ReadString(signal.LCode1);
-            string r2 = ReadString(signal.LCode2);
+            var r1 = client.ReadString(signal.LCode1);
+            var r2 = client.ReadString(signal.LCode2);
             return r1.PadLeft(MAX_LEN, '0') + r2.PadLeft(MAX_LEN, '0');            
         }
 
-        public static int ReadWeighingSignal(string slot) {
-            return ReadInt(param.ScanParam.GetWeigh);
+        public static int ReadWeighingSignal(IOpcClient client, OPCParam param) {
+            return client.ReadInt(param.ScanParam.GetWeigh);
         }
 
-        public static void WriteWeighingSignal(bool b) {
+        public static void WriteWeighingSignal(IOpcClient client, OPCParam param, bool b) {
             var val = b ? "0" : "2";
             client.Write(param.ScanParam.GetWeigh, val);
         }
 
-        public static void WriteACAreaCompletionSignal(LCodeSignal signal) {
+        public static void WriteACAreaCompletionSignal(IOpcClient client, LCodeSignal signal) {
             client.Write(signal.Signal, 0);
         }
 
-        public static string ReadCacheLabel() {
+        public static string ReadCacheLabel(IOpcClient client, OPCParam param) {
             const int MAX_LEN = 6;
-            string r1 = ReadString(param.CacheParam.BeforCacheLable1);
-            string r2 = ReadString(param.CacheParam.BeforCacheLable2);
+            var r1 = client.ReadString(param.CacheParam.BeforCacheLable1);
+            var r2 = client.ReadString(param.CacheParam.BeforCacheLable2);
             return r1.PadLeft(MAX_LEN, '0') + r2.PadLeft(MAX_LEN, '0');
         }
 
         /// <summary>
         /// 读布卷到达缓冲信号。
         /// </summary>
+        /// <param name="client">opc client</param>
+        /// <param name="param">opc param</param>
         /// <returns></returns>
-        public static bool ReadBeforeCacheSignal() {
-            return ReadBool(param.CacheParam.BeforCacheStatus);            
+        public static bool ReadBeforeCacheSignal(IOpcClient client, OPCParam param) {
+            return client.ReadBool(param.CacheParam.BeforCacheStatus);            
         }
 
-        public static void ResetBeforeCacheSignal() {
+        public static void ResetBeforeCacheSignal(IOpcClient client, OPCParam param) {
             client.Write(param.CacheParam.BeforCacheStatus, false);
         }
 
         /// <summary>
         /// 读扫描状态信号。
         /// </summary>
+        /// <param name="client">opc client.</param>
+        /// <param name="param">opc param.</param>
         /// <returns></returns>
-        public static bool ReadScanState() {
-            return ReadBool(param.ScanParam.ScanState);            
+        public static bool ReadScanState(IOpcClient client, OPCParam param) {
+            return client.ReadBool(param.ScanParam.ScanState);            
         }
 
         /// <summary>
@@ -92,7 +66,15 @@ namespace yidascan {
         /// <param name="label1">标签前半部分</param>
         /// <param name="label2">标签后半部分</param>
         /// <param name="camera">相机号</param>
-        public static void WriteScanOK(string area, string numb, string label1, string label2, string camera) {
+        /// <param name="client">opc client</param>
+        /// <param name="param">opc param</param>
+        public static void WriteScanOK(IOpcClient client, 
+            OPCParam param, 
+            string area, 
+            string numb, 
+            string label1, 
+            string label2, 
+            string camera) {
             // 交地
             client.Write(param.ScanParam.ToLocationArea, area);
             client.Write(param.ScanParam.ToLocationNo, numb);
@@ -102,13 +84,15 @@ namespace yidascan {
             // 相机
             client.Write(param.ScanParam.CameraNo, camera);
             // 完成信号。
-            WriteScanOK();
+            WriteScanOK(client, param);
         }
 
         /// <summary>
         /// 布卷条码扫描完成，令托架放下，传送带动。
         /// </summary>
-        public static void WriteScanOK() {
+        /// <param name="client">opc client instance.</param>
+        /// <param name="param">opc param</param>
+        public static void WriteScanOK(IOpcClient client, OPCParam param) {
             client.Write(param.ScanParam.ScanState, true);
         }
     }
