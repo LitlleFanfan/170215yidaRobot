97f821a3adbd522a185d34b72e7d6bb06bd92e1a
laozhang.gz <laozhang.gz@gmail.com>
2017-02-24 00:22:18 +0800

    #

--- src/yadascanrobot/yidascan/taskqueconf.cs
+++ src/yadascanrobot/yidascan/taskqueconf.cs
@@ -13,15 +13,15 @@ namespace yidascan {
     /// 用法：
     /// TaskQueConf<TaskQueues>.save(Application.StartupPath + "\\taskques.json", taskQue);
     /// or:
-    /// var ques = TaskQueConf<TaskQueues>.load(Application.StartupPath + "\\taskques.json");
+    /// var ques = TaskQueConf<TaskQueues>.load(Application.StartupPath + "\\taskques.json");    
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public static class TaskQueConf<T> {
         /// <summary>
         /// 把对象保存到json文件中。
         /// </summary>
-        /// <param name="path"></param>
-        /// <param name="obj"></param>
+        /// <param name="path">文件名</param>
+        /// <param name="obj">要保存的对象</param>
         public static void save(string path, T obj) {
             var s = JsonConvert.SerializeObject(obj);
             File.WriteAllText(path, s);
@@ -30,11 +30,15 @@ namespace yidascan {
         /// <summary>
         /// 从json文件中加载对象。
         /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
+        /// <param name="path">文件名</param>
+        /// <returns>加载失败会返回null</returns>
         public static T load(string path) {
-            var s = File.ReadAllText(path);
-            return JsonConvert.DeserializeObject<T>(s);
+            try {
+                var s = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<T>(s);
+            } catch (Exception) {
+                return default(T);
+            }
         }
     }
 }
