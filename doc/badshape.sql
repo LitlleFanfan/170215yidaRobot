283b9df34451b497cecd1fc1c67a4e21368c957f
laozhang.gz <laozhang.gz@gmail.com>
2017-03-20 01:29:07 +0800

    sql文件排错； 删除无用代码
    modified:   doc/badshape.sql
    modified:   src/yadascanrobot/yidascan/LableCodeBllPro.cs

--- doc/badshape.sql
+++ doc/badshape.sql
@@ -1,16 +1,16 @@
 create table BadShapeLocation (
        ToLocation varchar(256) not null primary key,
-       Slot varchar(256) not null default ""
+       Slot varchar(256) not null default ''
 );
 
-insert into table BadShapeLocation (ToLocation, Slot) values ("B01", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B02", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B03", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B04", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B05", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B06", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B07", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B08", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B09", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B10", "");
-insert into table BadShapeLocation (ToLocation, Slot) values ("B11", "");
+insert into BadShapeLocation (ToLocation, Slot) values ('B01', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B02', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B03', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B04', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B05', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B06', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B07', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B08', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B09', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B10', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B11', '');
