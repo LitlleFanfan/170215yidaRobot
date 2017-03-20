cdc775a20fb535f15c940d88da22b0e6e9796fec
lje <lje@LAPTOP-8HKU43OS>
2017-03-20 16:13:59 +0800

    111

--- doc/badshape.sql
+++ doc/badshape.sql
@@ -3,14 +3,14 @@ create table BadShapeLocation (
        Slot varchar(256) not null default ''
 );
 
-insert into BadShapeLocation (ToLocation, Slot) values ('B01', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B02', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B03', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B04', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B05', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B06', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B07', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B08', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B09', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B10', '');
-insert into BadShapeLocation (ToLocation, Slot) values ('B11', '');
+insert into BadShapeLocation (ToLocation, Slot) values ('B01', 'MicroWin.S7-1200.NewItem113');
+insert into BadShapeLocation (ToLocation, Slot) values ('B02', 'MicroWin.S7-1200.NewItem114');
+insert into BadShapeLocation (ToLocation, Slot) values ('B03', 'MicroWin.S7-1200.NewItem115');
+insert into BadShapeLocation (ToLocation, Slot) values ('B04', 'MicroWin.S7-1200.NewItem116');
+insert into BadShapeLocation (ToLocation, Slot) values ('B05', 'MicroWin.S7-1200.NewItem117');
+insert into BadShapeLocation (ToLocation, Slot) values ('B06', 'MicroWin.S7-1200.NewItem118');
+insert into BadShapeLocation (ToLocation, Slot) values ('B07', 'MicroWin.S7-1200.NewItem119');
+insert into BadShapeLocation (ToLocation, Slot) values ('B08', 'MicroWin.S7-1200.NewItem120');
+insert into BadShapeLocation (ToLocation, Slot) values ('B09', 'MicroWin.S7-1200.NewItem121');
+insert into BadShapeLocation (ToLocation, Slot) values ('B10', 'MicroWin.S7-1200.NewItem122');
+insert into BadShapeLocation (ToLocation, Slot) values ('B11', 'MicroWin.S7-1200.NewItem123');
