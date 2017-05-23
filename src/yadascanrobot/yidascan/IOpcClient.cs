048d838b7b59f42f0b53c8ddfc36081fc0bc0ffa
lje <lje@LAPTOP-8HKU43OS>
2017-05-23 14:46:54 +0800

    opc add group write.

--- src/yadascanrobot/yidascan/IOpcClient.cs
+++ src/yadascanrobot/yidascan/IOpcClient.cs
@@ -1,13 +1,21 @@
-﻿public interface IOpcClient {
+﻿using System.Collections.Generic;
+
+public interface IOpcClient {
     string ReadString(string slot);
     int ReadInt(string slot);
     bool ReadBool(string slot);
     decimal ReadDecimal(string slot);
+    string ReadString(string groupname, string slot);
+    int ReadInt(string groupname, string slot);
+    bool ReadBool(string groupname, string slot);
+    decimal ReadDecimal(string groupname, string slot);
     bool Write(string slot, object value);
+    bool Write(string groupname, Dictionary<string, object> codeValue);
     bool Set(string slot, object value);
     bool Open(string mAddr);
     void Close();
     void AddSubscription(System.Data.DataTable p);
     bool AddSubscription(string slot);
+    bool AddSubscriptions(string goupname, List<string> codes, int updateRate = 500);
 }
 
